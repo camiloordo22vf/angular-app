f5616ee925d92635d11dc485e268a65489d96e07
camiloordo22vf <v1vectorfile@gmail.com>
2022-10-13 09:49:26 -0500

    proyecto angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { RouterModule, Routes } from "@angular/router";
+import { AutorComponent } from "./components/utilidades/autor/autor.component";
+import { CaruselComponent } from "./components/utilidades/carusel/carusel.component";
+import { HeroesComponent } from "./components/utilidades/heroes/heroes.component";
+import { HomeComponent } from "./components/utilidades/home/home.component";
+import { SobrenComponent } from "./components/utilidades/sobren/sobren.component";
+
+const APP_ROUTES: Routes=[
+{path : 'home', component:HomeComponent},
+{path : 'heroes', component:HeroesComponent},
+{path : 'sobren', component:SobrenComponent},
+{path : 'autor', component:AutorComponent},
+{path : 'carusel', component:CaruselComponent},
+{path : 'fabricio', component:CaruselComponent},
+{path : '**', pathMatch:'full', redirectTo:'home'},
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES)
